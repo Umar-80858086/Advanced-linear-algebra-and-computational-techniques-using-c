45d7139d560ff49e9733fa8331addc7f4c5a03b6
Umar-808586 <210172220+Umar-80858086@users.noreply.github.com>
2026-02-22 15:47:56 +0530

    added one code

---
+++ sizeof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void main (){
+    char c[100000];
+    int x = sizeof(c);
+///// i just see how long diferrent dat tytoes are 
+    printf("%d\n", x);
+
+}
---
+++ union.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+union Data {
+    int i;       // 4 bytes
+    float f;     // 4 bytes  
+    char str[4]; // 4 bytes
+}; 
+
+
+// Data umar;
